e25163fdffa19e498a1b5f92bc0d264da1887929
seung-ik <69374857+seung-ik@users.noreply.github.com>
2021-04-15 14:04:49 +0900

    modify disableinput

--- src/pages/Disable/index.js
+++ src/pages/Disable/index.js
@@ -1,42 +1,17 @@
-import React, { useState, useCallback } from "react";
+import React, { useState } from "react";
 import "./style.css";
 
 const Disable = () => {
-  const [typingInputValue, setTypingInputValue] = useState("");
-  const [isLogin, setIsLogin] = useState(false);
+  const [isLogin] = useState(false);
 
-  const onChangeTypingInputValue = useCallback((e) => {
-    setTypingInputValue(e.target.value);
-  }, []);
-  const changeLogin = useCallback(() => {
-    setIsLogin((prev) => !prev);
-  }, []);
-
-  if (isLogin) {
-    return (
-      <div style={{ display: "flex", flexDirection: "column", margin: "30px" }}>
-        <div className="input-container">
-          <div className="input-outterbox">
-            <textarea className="input-innerbox" type="text" placeholder="주문사항을 입력해주세요" maxLength={300} onChange={onChangeTypingInputValue} />
-            <div className="input-maxlength">{300 - typingInputValue.length}</div>
-          </div>
-          <button className="save-button">SAVE</button>
-        </div>
-        <button className="userbutton" onClick={changeLogin}>
-          logout
-        </button>
-      </div>
-    );
-  }
   return (
     <div style={{ display: "flex", flexDirection: "column", margin: "30px" }}>
-      <div className="disable-box">
-        로그인이 필요한 서비스 입니다.
-        <span>500</span>
+      <div className="disable-outterbox">
+        <textarea disabled={!isLogin} className="disable-box">
+          로그인이 필요한 서비스입니다.
+        </textarea>
+        <div className="disable-maxlength">500</div>
       </div>
-      <button className="userbutton" onClick={changeLogin}>
-        login
-      </button>
     </div>
   );
 };
